f5160fd23846fc178eefde5e3aec928b67121041
Garett Breen <garett.breen@gmail.com>
2022-03-31 13:45:21 -0600

    Use https

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["EsriSwiftUI"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:Esri/arcgis-runtime-ios.git", from: "100.10.0")
+        .package(url: "https://github.com/Esri/arcgis-runtime-ios.git", from: "100.10.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
