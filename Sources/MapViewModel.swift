bfb63a67c15f328cf39c4980207d7cc2b50ca61b
Garett Breen <garett.breen@gmail.com>
2022-04-01 11:32:20 -0600

    Move map view model

---
+++ Sources/MapViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Garett Breen on 4/1/22.
+//
+
+import ArcGIS
+import Combine
+import SwiftUI
+
+open class MapViewModel: ObservableObject {
+    var subscriptions = Set<AnyCancellable>()
+    @Published public var map: AGSMap
+    @Published public var graphicsOverlays: [AGSGraphicsOverlay]
+    @Published public var isAttributionTextVisible: Bool
+    @Published public var errorGettingLocation = false
+    
+    public let zoomToGeometry = PassthroughSubject<GeometryZoom, Never>()
+    public let zoomToViewpoint = PassthroughSubject<AGSViewpoint, Never>()
+    public let zoomToCurrentLocation = PassthroughSubject<Void, Never>()
+    
+    public init(map: AGSMap = AGSMap(basemap: .openStreetMap()), graphicsOverlays: [AGSGraphicsOverlay] = [], isAttributionTextVisible: Bool = true) {
+        self.map = map
+        self.graphicsOverlays = graphicsOverlays
+        self.isAttributionTextVisible = isAttributionTextVisible
+    }
+    
+    open func pointTapped(screenPoint: CGPoint, mapPoint: AGSPoint) {
+        
+    }
+}
